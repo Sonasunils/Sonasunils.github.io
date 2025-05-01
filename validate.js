302d382b08575c9c061b90bce0bae15913145ae8
Sonasunils <148698303+Sonasunils@users.noreply.github.com>
2025-05-01 23:05:40 +0530

    Add files via upload

--- validate.js
+++ validate.js
@@ -1,10 +1,11 @@
+// In login page (validate.js)
 function validate() {
     const username = document.login.username.value.trim();
     const password = document.login.password.value;
   
     if (username === "aryan123" && password === "aryannew") {
-      sessionStorage.setItem("username", username);
-      window.location.href = "welcome.html"; // no 405, just JS redirect
+      sessionStorage.setItem("username", username);  // Store username in sessionStorage
+      window.location.href = "welcome.html"; // Redirect to welcome page
       return false;
     } else {
       alert("Incorrect username or password.");
