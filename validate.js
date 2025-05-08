cd66ec954341a7c618c82ca326b7b002d0d189f7
Sonasunils <148698303+Sonasunils@users.noreply.github.com>
2025-05-09 00:13:01 +0530

    Add files via upload

--- validate.js
+++ validate.js
@@ -1,15 +1,2 @@
-// In login page (validate.js)
-function validate() {
-    const username = document.login.username.value.trim();
-    const password = document.login.password.value;
-  
-    if (username === "aryan123" && password === "aryannew") {
-      sessionStorage.setItem("username", username);  // Store username in sessionStorage
-      window.location.href = "welcome.html"; // Redirect to welcome page
-      return false;
-    } else {
-      alert("Incorrect username or password.");
-      return false;
-    }
-  }
+
   
